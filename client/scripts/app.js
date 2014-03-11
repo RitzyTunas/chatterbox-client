056930aa5410f0fd1a2d3ba25c9963677c4d76ec
Hack Reactor Students <7riste@gmail.com>
2014-03-10 17:31:18 -0700

    added chat display.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,49 +7,58 @@ app.init = function() {
 
 };
 
-app.send = function(message) {
-//   $.ajax({
-//   // always use this url
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
-};
-
 app.fetch = function() {
   $.ajax({
-      url: app.server,
+      url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
       dataType: 'json',
       success: function(data) {
         for (var i=0; i<data.results.length; i++) {
           var message = data.results[i];
           app.addMessage(message);
-    }
-  },
-    error: function () {
-      console.error('chatterbox: Failed to send message');
-    }
+        }
+      },
+      error: function () {
+        console.error('chatterbox: Failed to get messages');
+      }
   });
 };
 
+app.send = function(message) {
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+};
+
 app.addMessage = function(message) {
-  $('#chats').append('<li>' + message.text + '</li>');
+  var username = message.username;
+  var text = message.text;
+  var roomname = message.roomname;
+  var createdat = message.createdAt;
+  var updatedat = message.updatedAt;
+  $('#chats').append('<li>' + username + ': ' + text + ' ' + roomname + ' ' + createdat  + ' ' + updatedat + '</li>');
 };
 
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
-
-
+var message = {
+  'username': 'shawndrost',
+  'text': 'jellyfish',
+  'roomname': '4chan'
+};
+app.send(message);
 app.fetch();
 
